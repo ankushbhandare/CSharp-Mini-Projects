ce89b4dfc7949cd3f1f4a00b109c6c70b67a0dc5
ankushbhandare <bhandare_ankush@rediffmail.com>
2019-03-27 17:46:17 -0700

    Added Maths Quiz Application V1.0

---
+++ Maths Quiz/Maths Quiz/Quiz.Designer.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace Maths_Quiz
+{
+    partial class Quiz
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblQuestion = new System.Windows.Forms.Label();
+            this.txtAnswer = new System.Windows.Forms.TextBox();
+            this.btnSubmitAnswer = new System.Windows.Forms.Button();
+            this.lblResult = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // lblQuestion
+            // 
+            this.lblQuestion.AutoSize = true;
+            this.lblQuestion.Location = new System.Drawing.Point(200, 70);
+            this.lblQuestion.Name = "lblQuestion";
+            this.lblQuestion.Size = new System.Drawing.Size(47, 13);
+            this.lblQuestion.TabIndex = 0;
+            this.lblQuestion.Text = "question";
+            // 
+            // txtAnswer
+            // 
+            this.txtAnswer.AcceptsReturn = true;
+            this.txtAnswer.Location = new System.Drawing.Point(203, 103);
+            this.txtAnswer.Name = "txtAnswer";
+            this.txtAnswer.Size = new System.Drawing.Size(100, 20);
+            this.txtAnswer.TabIndex = 1;
+            this.txtAnswer.KeyUp += new System.Windows.Forms.KeyEventHandler(this.txtAnswer_KeyUp);
+            // 
+            // btnSubmitAnswer
+            // 
+            this.btnSubmitAnswer.Location = new System.Drawing.Point(203, 152);
+            this.btnSubmitAnswer.Name = "btnSubmitAnswer";
+            this.btnSubmitAnswer.Size = new System.Drawing.Size(100, 23);
+            this.btnSubmitAnswer.TabIndex = 2;
+            this.btnSubmitAnswer.Text = "Submit Answer";
+            this.btnSubmitAnswer.UseVisualStyleBackColor = true;
+            this.btnSubmitAnswer.Click += new System.EventHandler(this.btnSubmitAnswer_Click);
+            // 
+            // lblResult
+            // 
+            this.lblResult.AutoSize = true;
+            this.lblResult.Location = new System.Drawing.Point(200, 232);
+            this.lblResult.Name = "lblResult";
+            this.lblResult.Size = new System.Drawing.Size(0, 13);
+            this.lblResult.TabIndex = 3;
+            // 
+            // Quiz
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.lblResult);
+            this.Controls.Add(this.btnSubmitAnswer);
+            this.Controls.Add(this.txtAnswer);
+            this.Controls.Add(this.lblQuestion);
+            this.Name = "Quiz";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblQuestion;
+        private System.Windows.Forms.TextBox txtAnswer;
+        private System.Windows.Forms.Button btnSubmitAnswer;
+        private System.Windows.Forms.Label lblResult;
+    }
+}
+
---
+++ Maths Quiz/Maths Quiz/Quiz.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Maths_Quiz
+{
+    public partial class Quiz : Form
+    {
+        Random random = new Random();
+        int answer;
+        int totalCount = 0;
+        int correctCount = 0;
+        public Quiz()
+        {
+            InitializeComponent();
+            GenerateQuiz();
+        }
+
+        private void GenerateQuiz()
+        {
+            GenerateQuestion();
+        }
+
+        private void GenerateQuestion()
+        {
+            txtAnswer.Clear();
+            int number1 = random.Next(11);
+            int number2 = random.Next(11);
+            int operation=random.Next(4);
+            try
+            {
+                switch (operation)
+                {
+                    case 0:
+                        lblQuestion.Text = number1.ToString() + " + " + number2.ToString() + " = ?";
+                        answer = number1 + number2;
+                        break;
+                    case 1:
+                        lblQuestion.Text = number1.ToString() + " - " + number2.ToString() + " = ?";
+                        answer = number1 - number2;
+                        break;
+                    case 2:
+                        lblQuestion.Text = number1.ToString() + " * " + number2.ToString() + " = ?";
+                        answer = number1 * number2;
+                        break;
+                    case 3:
+                        if (number2 == 0)
+                            number2 = random.Next(1, 11);
+                        lblQuestion.Text = (number1*number2).ToString() + " / " + number2.ToString() + " = ?";
+                        answer = number1;
+                        break;
+                    default:
+                        throw new NotImplementedException();
+                }
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+
+        private void btnSubmitAnswer_Click(object sender, EventArgs e)
+        {
+            int userAnswer;
+            try
+            {
+                if(!int.TryParse(txtAnswer.Text, out userAnswer))
+                {
+                    throw new ArgumentException();
+                }
+                if(userAnswer == answer)
+                {
+                    lblResult.Text = "Correct Answer";
+                    correctCount++;
+                }
+                else
+                {
+                    lblResult.Text = "Incorrect Answer";
+                }
+                totalCount++;
+                GenerateQuestion();
+            }
+            catch(ArgumentException ex)
+            {
+                MessageBox.Show("Please Enter Numbers Only!");
+            }
+        }
+
+        private void txtAnswer_KeyUp(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Enter)
+                btnSubmitAnswer_Click(sender, e);
+        }
+    }
+}
